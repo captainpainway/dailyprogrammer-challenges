c8d6274bf33fceb9c8af3366fd029176a1ff2e79
captainpainway <mary.c.staggs@gmail.com>
2016-01-30 20:30:07 -0500

    Finished decoder

--- challenge-245/script.js
+++ challenge-245/script.js
@@ -1,22 +1,52 @@
 "use strict"
 
-let key = "a GgG d GggGg e GggGG g GGGgg h GGGgG i GGGGg l GGGGG m ggg o GGg p Gggg r gG y ggG"
-let message = "GGGgGGGgGGggGGgGggG /gG/GggGgGgGGGGGgGGGGGggGGggggGGGgGGGgggGGgGggggggGggGGgG!"
+let key = "a GgG d GggGg e GggGG g GGGgg h GGGgG i GGGGg l GGGGG m ggg o GGg p Gggg r gG y ggG";
+let message = "GGGgGGGgGGggGGgGggG /gG/GggGgGgGGGGGgGGGGGggGGggggGGGgGGGgggGGgGggggggGggGGgG!";
+let key2 = "H GgG d gGg e ggG l GGg o gGG r Ggg w ggg";
+let message2 = "GgGggGGGgGGggGG, ggggGGGggGGggGg!";
 
-class Decode {
-    constructor() {
-        this.map = new Map();
+class KeyCreate {
+    constructor(item, toDecode) {
+        this.obj = {};
+        this.key = item.split(' ');
     }
-    createObj(key) {
+    createObj() {
         let y = 0;
-        key = key.split(' ');
-        key.map((letter,x) => {
+        this.key.map((letter,x) => {
             y++;
-            !(x % 2) ? this.map[key[y]] = letter : '';
+            !(x % 2) ? this.obj[this.key[y]] = letter : '';
         });
-        console.log(this.map);
     }
 }
 
-let decoded = new Decode(key);
-decoded.createObj(key);
+class Decode extends KeyCreate {
+    constructor(item, toDecode) {
+        super(item, toDecode);
+        this.decoded = '';
+        this.message = toDecode.split('');
+    }
+    decode() {
+        super.createObj();
+        let word = '';
+        this.message.forEach(x => {
+            if(x == 'G' || x == 'g'){
+                word += x;
+            }else{
+                this.decoded += x;
+                word = '';
+            }
+            Object.keys(this.obj).forEach(y => {
+                if(word == y){
+                    this.decoded += this.obj[y];
+                    word = '';
+                }
+            });
+        });
+        console.log(this.decoded);
+    }
+}
+
+let decoded = new Decode(key2, message2);
+decoded.decode();
+decoded = new Decode(key, message);
+decoded.decode();
